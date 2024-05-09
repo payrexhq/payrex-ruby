6cc7bee008ec2b5645caded80ce9857a2302504f
josephisleta <jjosephisleta@gmail.com>
2024-05-09 14:57:06 +0800

    Implement verify webhook signature

---
+++ lib/entities/event.rb
@@ -0,0 +1,23 @@
+module Payrex
+  module Entities
+    class Event
+      attr_reader :id,
+                  :data,
+                  :type,
+                  :pending_webhooks,
+                  :previous_attributes,
+                  :created_at,
+                  :updated_at
+
+      def initialize(api_resource)
+        @id = api_resource.data["id"]
+        @data = api_resource.data["data"]
+        @type = api_resource.data["type"]
+        @pending_webhooks = api_resource.data["pending_webhooks"]
+        @previous_attributes = api_resource.data["previous_attributes"]
+        @created_at = api_resource.data["created_at"]
+        @updated_at = api_resource.data["updated_at"]
+      end
+    end
+  end
+end
